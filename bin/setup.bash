ce4f65b6682449a970e3d7a86ed01d3b90170d19
Ian Ward <ianshward@gmail.com>
2014-01-28 18:08:44 -0500

    sudoers

--- bin/setup.bash
+++ bin/setup.bash
@@ -26,6 +26,8 @@ sed -e "s/%USER/$user/g" -e "s/%PORT/$port/g" < ./etc/ssh/sshd_config > $config_
 sed "s/%PORT/$port/g" ./etc/iptables.txt > $config_path/iptables.txt
 echo "net.ipv4.netfilter.ip_conntrack_max=262144" >> $config_path/sysctl.conf
 echo "net.ipv4.ip_local_port_range=10000 65535" >> $config_path/sysctl.conf
+echo "$user ALL=NOPASSWD: ALL" > $config_path/sudoers.d/60_$user
+chmod 440 $config_path/sudoers.d/60_$user
 
 if [ "$op" = "install" ]; then
     restart ssh
